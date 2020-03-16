c16c90712f529aab63aecfe6155fc1ae97afbedf
Hollownett <50251199+Hollownett@users.noreply.github.com>
2020-03-16 16:27:07 +0300

    Update User.js

--- src/components/Message/User.js
+++ src/components/Message/User.js
@@ -13,16 +13,16 @@ class User extends Component {
                         <div className="nim-dialog--content">
                             <div className="nim-dialog--cw">
                                 <div className="nim-dialog--date ">
-                                    20:07
+                                    00:00
                                 </div>
                                 <div className="nim-dialog--name">
-                                    <span className="nim-dialog--name-w">Mekan Nepesow</span>
+                                    <span className="nim-dialog--name-w">User1</span>
                                 </div>
                                 <div className="nim-dialog--text-preview">
-                                    <span className="">Bolya</span>
+                                    <span className="">Message text</span>
                                 </div>
                                 <div className="blind_label">
-                                    1 новое сообщение
+                                    1 new message
                                 </div>
                                 <div className="unread nim-dialog--unread">
                                     1
